45d8d95f64dd6b12dea01f625c3072b120256449
zacario-li <lzjcyy@qq.com>
2025-05-25 21:09:28 +0800

    reformat build script

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -3,12 +3,6 @@ REM ===== Nuitka 打包脚本 =====
 REM 需先激活好 Python 环境并安装好 Nuitka、PySide6、edge-tts、pyttsx3 等依赖
 REM 自动生成的ico文件名为 app_icon_auto.ico
 
-python -m nuitka ^
-  --onefile ^
-  --windows-console-mode=disable ^
-  --windows-icon-from-ico=app_icon_auto.ico ^
-  --enable-plugin=pyside6 ^
-  --include-qt-plugins=all ^
-  word_announcer.py
+python -m nuitka --onefile --windows-console-mode=disable --windows-icon-from-ico=app_icon_auto.ico --enable-plugin=pyside6 --include-qt-plugins=all word_announcer.py
 
 pause 
